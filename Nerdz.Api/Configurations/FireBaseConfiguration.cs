8453f4db90b32eb8da5dfc4f107f936fde1150d3
Giovanni Brandolise <92755235+GiovanniBrand@users.noreply.github.com>
2025-11-06 16:28:08 -0300

    Feature: Cria estrutura inicial do projeto Nerdzz configurando mediatr, fluentvalidator e firebase (#1)
    Co-authored-by: Giovanni Brandolise <giovanni.brandolise@mps.com.br>

---
+++ Nerdz.Api/Configurations/FireBaseConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Google.Cloud.Firestore;
+
+namespace Nerdz.Api.Configurations
+{
+    public static class FireBaseConfiguration
+    {
+        public static void ConfigureFireBase(this WebApplicationBuilder builder)
+        {
+            var firebaseProjectId = builder.Configuration["Firebase:ProjectId"];
+            var credentialPath = builder.Configuration["Firebase:CredentialPath"];
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
+            FirestoreDb firestore = FirestoreDb.Create(firebaseProjectId);
+            builder.Services.AddSingleton(firestore);
+        }
+    }
+}
---
+++ Nerdz.Api/Configurations/MediatrConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+using Nerdz.Application.Common;
+using Nerdz.Application.Validators;
+
+namespace Nerdz.Api.Configurations
+{
+    public static class MediatrConfigurations
+    {
+        public static void ConfigureMediatr(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(ApplicationAssemblyReference).Assembly);
+            });
+        }
+
+        public static void ConfigureFluentValidator(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddValidatorsFromAssembly(typeof(ApplicationAssemblyReference).Assembly);
+            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(FluentValidatorBehavior<,>));
+        }
+    }
+}
